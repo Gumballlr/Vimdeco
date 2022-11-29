6b841ff4a3dfffcf07f7360640a630b6335e6780
unknown <ducminhdmdm@gmail.com>
2022-11-29 17:50:00 +0700

    home screen

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,126 @@
+  <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
+
+    <div class="brand-logo">
+
+      <h2 class="logo-text">Vimadeco</h2>
+      <div class="close-btn"><i class="zmdi zmdi-close"></i></div>
+    </div>
+
+    <ul class="metismenu" id="menu">
+      <li>
+        <a href="javascript:void();">
+          <div class="parent-icon">
+            <i class="zmdi zmdi-view-dashboard"></i>
+          </div>
+          <div class="menu-title">TRANG CHỦ</div>
+        </a>
+
+      </li>
+
+
+
+      <li>
+        <a class="has-arrow" href="javascript:void();">
+          <div class="parent-icon"> <i class='zmdi zmdi-card-travel'></i></div>
+          <div class="menu-title">QUẢN LÝ TÀI KHOẢN</div>
+        </a>
+        <ul>
+          <li><a href="components-range-slider.html"><i class="zmdi zmdi-dot-circle-alt"></i> THÔNG TIN TÀI KHOẢN</a>
+          </li>
+          <li><a href="components-image-carousel.html"><i class="zmdi zmdi-dot-circle-alt"></i> PHẦN QUYỀN HỆ
+              THỐNG</a>
+          </li>
+
+
+
+        </ul>
+      </li>
+
+      <li>
+        <a class="has-arrow" href="javascript:void();">
+          <div class="parent-icon"> <i class='zmdi zmdi-chart'></i></div>
+          <div class="menu-title">QUẢN LÝ CONTAINER</div>
+        </a>
+        <ul>
+          <li><a href="charts-chartjs.html"><i class="zmdi zmdi-dot-circle-alt"></i> QUẢN LÝ CLEARANCE</a></li>
+          <li><a href="charts-apex.html"><i class="zmdi zmdi-dot-circle-alt"></i>GATE IN/OUT</a></li>
+          <li><a href="charts-sparkline.html"><i class="zmdi zmdi-dot-circle-alt"></i> QUẢN LÝ DỮ LIỆU</a></li>
+
+        </ul>
+      </li>
+
+      <li>
+        <a href="javascript:void();">
+          <div class="parent-icon"> <i class='zmdi zmdi-widgets'></i></div>
+          <div class="menu-title">SẢN LƯỢNG XE NÂNG HẠ</div>
+        </a>
+
+      </li>
+
+      <li>
+        <a class="has-arrow" href="javascript:void();">
+          <div class="parent-icon"> <i class='zmdi zmdi-email'></i></div>
+          <div class="menu-title">KIỂM SOÁT VẬT TƯ</div>
+        </a>
+        <ul>
+          <li><a href="mail-inbox.html"><i class="zmdi zmdi-dot-circle-alt"></i> QUẢN LÝ VẬT TƯ</a></li>
+          <li><a href="mail-compose.html"><i class="zmdi zmdi-dot-circle-alt"></i> NHẬP XUẤT VẬT TƯ</a></li>
+
+        </ul>
+      </li>
+      <li>
+        <a class="has-arrow" href="javascript:void();">
+          <div class="parent-icon"> <i class='zmdi zmdi-format-list-bulleted'></i></div>
+          <div class="menu-title">QUẢN LÝ KHO DẦU </div>
+        </a>
+        <ul>
+          <li><a href="form-inputs.html"><i class="zmdi zmdi-dot-circle-alt"></i> QUẢN LÝ TRẠM DẦU</a></li>
+          <li><a href="form-input-group.html"><i class="zmdi zmdi-dot-circle-alt"></i>NHẬP XUẤT DẦU</a></li>
+
+        </ul>
+      </li>
+
+      <li>
+        <a class="has-arrow" href="javascript:void();">
+          <div class="parent-icon"> <i class='zmdi zmdi-lock'></i></div>
+          <div class="menu-title">QUẢN LÝ ĐỘI XE</div>
+        </a>
+        <ul>
+          <li><a href="authentication-signin.html" target="_blank"><i class="zmdi zmdi-dot-circle-alt"></i> PHƯƠNG
+              TIỆN VÀ ĐỘI XE
+            </a></li>
+          <li><a href="authentication-signup.html" target="_blank"><i class="zmdi zmdi-dot-circle-alt"></i> TUYẾN
+              ĐƯỜNG VÀ CHI PHÍ</a></li>
+          <li><a href="authentication-signin2.html" target="_blank"><i class="zmdi zmdi-dot-circle-alt"></i> HOẠT ĐỘNG
+              VÀ KHAI THÁC</a></li>
+          <li><a href="authentication-signup2.html" target="_blank"><i class="zmdi zmdi-dot-circle-alt"></i> CHI PHÍ
+              KHÔNG CỐ ĐỊNH </a></li>
+          <li><a href="authentication-lock-screen.html" target="_blank"><i class="zmdi zmdi-dot-circle-alt"></i> BÁO
+              CÁO HOẠT ĐỘNG</a></li>
+
+        </ul>
+      </li>
+
+      <li><a href="calendar.html">
+          <div class="parent-icon"><i class='zmdi zmdi-calendar-check'></i></div>
+          <div class="menu-title">BÁC CÁO TÀI CHÍNH</div>
+          <div class="badge badge-light ml-auto">Mới</div>
+        </a></li>
+
+
+      <!-- <li class="menu-label">Labels</li>
+        <li><a href="javascript:void();">
+            <div class="parent-icon"><i class='zmdi zmdi-coffee'></i></div>
+            <div class="menu-title">Important</div>
+          </a></li>
+        <li><a href="javascript:void();">
+            <div class="parent-icon"><i class='zmdi zmdi-chart-donut'></i></div>
+            <div class="menu-title">Warning</div>
+          </a></li>
+        <li><a href="javascript:void();">
+            <div class="parent-icon"><i class='zmdi zmdi-share'></i></div>
+            <div class="menu-title">Information</div>
+          </a></li> -->
+    </ul>
+
+  </div>
---
+++ resources/views/layout/topbar.blade.php
@@ -0,0 +1,68 @@
+  <header class="topbar-nav">
+    <nav class="navbar navbar-expand fixed-top">
+
+      <div class="toggle-menu">
+        <i class="zmdi zmdi-menu"></i>
+      </div>
+
+      <!--- search form -->
+      <!-- <div class="search-bar flex-grow-1">
+          <div class="input-group">
+            <div class="input-group-prepend search-arrow-back">
+              <button class="btn btn-search-back" type="button"><i class="zmdi zmdi-long-arrow-left"></i></button>
+            </div>
+            <input type="text" class="form-control" placeholder="search">
+            <div class="input-group-append">
+              <button class="btn btn-search" type="button"><i class="zmdi zmdi-search"></i></button>
+            </div>
+          </div>
+        </div> -->
+
+      <ul class="navbar-nav align-items-center right-nav-link ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" data-toggle="dropdown"
+            href="javascript:void();">
+            <span class="user-profile"><img src="assets/images/avatars/avatar-13.png" class="img-circle"
+                alt="user avatar"></span>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-right">
+            <li class="dropdown-item user-details">
+              <a href="javaScript:void();">
+                <div class="media">
+                  <div class="avatar"><img class="align-self-start mr-3" src="assets/images/avatars/avatar-13.png"
+                      alt="user avatar"></div>
+                  <div class="media-body">
+                    <h6 class="mt-2 user-title">Nguyễn Đức Minh</h6>
+                    <p class="user-subtitle">dm@gmail.com</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="dropdown-divider"></li>
+            <li class="dropdown-item"><i class="zmdi zmdi-comments mr-3"></i>Inbox</li>
+            <li class="dropdown-divider"></li>
+            <li class="dropdown-item"><i class="zmdi zmdi-balance-wallet mr-3"></i>Account</li>
+            <li class="dropdown-divider"></li>
+            <li class="dropdown-item"><i class="zmdi zmdi-settings mr-3"></i>Setting</li>
+            <li class="dropdown-divider"></li>
+            <li class="dropdown-item"><i class="zmdi zmdi-power mr-3"></i>Logout</li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a style="color:black;font-size:18px">Nguyễn Đức Minh</a>
+        </li>
+
+        <li class=" nav-item dropdown language">
+          <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" data-toggle="dropdown"
+            href="javascript:void();"><i class="flag-icon flag-icon-gb align-middle"></i></a>
+          <ul class="dropdown-menu dropdown-menu-right">
+            <li class="dropdown-item"><i class="flag-icon flag-icon-gb mr-3"></i>English</li>
+            <li class="dropdown-item"><i class="flag-icon flag-icon-fr mr-3"></i>French</li>
+            <li class="dropdown-item"><i class="flag-icon flag-icon-cn mr-3"></i>Chinese</li>
+            <li class="dropdown-item"><i class="flag-icon flag-icon-de mr-3"></i>German</li>
+          </ul>
+        </li>
+
+      </ul>
+    </nav>
+  </header>
